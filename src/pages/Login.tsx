a073f36ba3bc3cc7a170ef626d40c868d5062f85
Darrien Park <park.darrien@gmail.com>
2025-01-30 21:25:16 -0500

    pre-load dog walk image

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,12 +2,21 @@ import { Card, CardContent } from "@mui/joy";
 import bg from "../assets/dogwalkbackground.webp";
 import Layout from "../components/Layout";
 import LoginForm from "../components/LoginForm";
+import { useState, useEffect } from "react";
 
 const LoginPage = () => {
+  const [bgLoaded, setBgLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = bg;
+    img.onload = () => setBgLoaded(true);
+  }, []);
+
   return (
     <div
       className="flex flex-col flex-grow justify-center items-center bg-cover bg-center"
-      style={{ backgroundImage: `url(${bg})` }}
+      style={{ backgroundImage: bgLoaded ? `url(${bg})` : "none" }}
     >
       <Layout>
         <Card className="w-full max-w-md m-auto">
